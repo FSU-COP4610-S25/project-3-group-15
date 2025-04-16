d48d10c28ab8c7474483bfd334d99e0dd3dbb333
Jordan <jfreyman11037@gmail.com>
2025-04-16 14:18:11 -0400

    trying to fix entries in one fat

--- include/fat32.h
+++ include/fat32.h
@@ -4,23 +4,47 @@
 #include <stdint.h>
 #include <stdio.h>
 
-// Struct to store FAT32 boot sector fields
+#pragma pack(push, 1)
 typedef struct {
-    uint16_t bytes_per_sector;  // Bytes per sector
-    uint8_t sectors_per_cluster; // Sectors per cluster
-    uint16_t reserved_sector_count; // Reserved sectors (including boot sector)
-    uint8_t num_fats;           // Number of FATs
-    uint32_t total_sectors;      // Total sectors in file system
-    uint32_t fat_size;          // Size of one FAT (in sectors)
-    uint32_t root_cluster;      // First cluster of the root directory
-    uint32_t total_clusters;    // Total clusters in data region (calculated)
+    uint8_t jump[3];              // 0x00
+    uint8_t oem[8];               // 0x03
+    uint16_t bytes_per_sector;   // 0x0B
+    uint8_t sectors_per_cluster; // 0x0D
+    uint16_t reserved_sector_count; // 0x0E
+    uint8_t num_fats;            // 0x10
+    uint16_t root_entry_count;   // 0x11 (unused in FAT32)
+    uint16_t total_sectors_16;   // 0x13
+    uint8_t media;               // 0x15
+    uint16_t fat_size_16;        // 0x16 (unused in FAT32)
+    uint16_t sectors_per_track;  // 0x18
+    uint16_t num_heads;          // 0x1A
+    uint32_t hidden_sectors;     // 0x1C
+    uint32_t total_sectors_32;   // 0x20
+    uint32_t fat_size_32;        // 0x24
+    uint16_t ext_flags;          // 0x28
+    uint16_t fs_version;         // 0x2A
+    uint32_t root_cluster;       // 0x2C
+    // (You can include more fields if needed)
+} BPB;
+#pragma pack(pop)
+
+typedef struct {
+    uint16_t bytes_per_sector;
+    uint8_t sectors_per_cluster;
+    uint16_t reserved_sector_count;
+    uint8_t num_fats;
+    uint32_t total_sectors;
+    uint32_t fat_size;
+    uint32_t root_cluster;
+    uint32_t total_clusters;
+    uint32_t fat_entries;
 } FAT32;
 
-// Function declarations
 int read_boot_sector(FILE *image, FAT32 *fs);
 void print_fat32_info(FAT32 *fs);
 
 #endif
+
 
 //FIANAS ATTEMPT 
 // #ifndef FAT32_H
